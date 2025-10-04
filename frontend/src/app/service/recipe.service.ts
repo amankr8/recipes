860eaed807f02959b25678392ffdf7babb7c4061
amankr8 <amankumar141099@gmail.com>
2025-10-04 13:10:19 +0530

    git update

--- frontend/src/app/service/recipe.service.ts
+++ frontend/src/app/service/recipe.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class RecipeService {
-  private apiUrl = environment.apiUrl + '/recipes';
+  private apiUrl = environment.apiUrl + '/api/recipes';
 
   constructor(private http: HttpClient) {}
 
