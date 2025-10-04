54b83982e321a5170c415567ba360e94fa8daae9
amankr8 <amankumar141099@gmail.com>
2025-10-04 15:00:03 +0530

    Swagger docs added

--- backend/src/main/java/com/recipe/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/recipe/backend/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.recipe.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -10,13 +14,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info(
-                new io.swagger.v3.oas.models.info.Info()
+                new Info()
                         .title("Recipe API")
                         .version("1.0")
                         .description("API documentation for the Recipe application")
         ).servers(
-                java.util.List.of(
-                        new io.swagger.v3.oas.models.servers.Server().url("http://localhost:8080").description("Local Server")
+                List.of(
+                    new Server()
+                        .url("http://localhost:8080")
+                        .description("Local Server")
                 )
         );
     }
