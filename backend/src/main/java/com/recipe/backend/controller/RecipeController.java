769281d5885f05ff9e945e7564e602b52e2e8ef9
amankr8 <amankumar141099@gmail.com>
2025-10-04 15:54:35 +0530

    Swagger docs added

--- backend/src/main/java/com/recipe/backend/controller/RecipeController.java
+++ backend/src/main/java/com/recipe/backend/controller/RecipeController.java
@@ -1,5 +1,6 @@
 package com.recipe.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -16,12 +17,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface RecipeController {
 
     @GetMapping
+    @Operation(summary = "Get all recipes", description = "Fetch a list of all recipes")
     ResponseEntity<?> getAllRecipes();
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get recipe by ID", description = "Fetch a single recipe by its ID")
     ResponseEntity<?> getRecipeById(@PathVariable Long id);
 
     @GetMapping("/search")
+    @Operation(summary = "Search recipes by query", description = "Search for recipes based on a query string")
     ResponseEntity<?> searchRecipesByQuery(
             @RequestParam
             @NotBlank(message = "Query parameter cannot be blank")
